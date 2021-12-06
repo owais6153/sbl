dd8c330310094f4606cd684bf6e8dd77e7567200
owais6153 <owais6153@gmail.com>
2021-12-06 22:25:20 +0500

    Scheduler timing change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('importItems:hourly')->hourly();
-        $schedule->command('replen:import')->everySixHours();
+        $schedule->command('replen:import')->daily();
     }
 
     /**
